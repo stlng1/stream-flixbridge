c8d08f3615719dd984ccfc212542fd48f2200dbb
stlng1 <femi@strangenig.com>
2026-05-19 21:02:07 +0100

    Initial commit

---
+++ master.js
@@ -0,0 +1,34 @@
+// =============================================
+// Page Configuration
+// =============================================
+// Set these variables for each landing page.
+
+// ClickHub URL
+var offerClickUrl = "https://marjoramvale.com/click";
+
+// AMPS Campaign URL
+var ampsUrl =
+  "https://marjoramvale.com/t/af0cb35e-1965-4217-ab75-a7a7e9650c66";
+
+var entryAlert = false; // false or "Your alert message here"
+var secondChanceAlert = false; // false or "Your alert message here"
+var outclickAlert = false; // false or "Your alert message here"
+var deviceVibration = false; // true or false
+var flashlight = false; // true or false
+
+
+var campaignConfig = {
+	"CAMPAIGN ID HERE": {
+		secondChanceAlert: false,
+		deviceVibration: true
+	},
+	"CAMPAIGN ID HERE": {
+		secondChanceAlert: "MESSAGE HERE",
+		deviceVibration: true
+	},
+	"CAMPAIGN ID HERE": {
+		outclickAlert: "MESSAGE HERE",
+		entryAlert: "Hello!",
+		flashlight: true
+	}
+}
